065e73cb05f74ab18dcbfc74a73e410ddfc63a6d
JunYoungJee <rainmaker8492@gmail.com>
2022-01-20 01:33:35 +0900

    58 cate presenter unit test classes and test method

---
+++ Udemy_UnitTestingSwiftMobileAppTests/SignUp/SignUpPresenterTests.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpPresenterTests.swift
+//  Udemy_UnitTestingSwiftMobileAppTests
+//
+//  Created by pablo.jee on 2022/01/20.
+//
+
+import XCTest
+
+class SignUpPresenterTests: XCTestCase {
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+    func testSignupPresenter_WhenInformationProvided_WillValidateEachProtperty() {
+        //Arrange
+        
+        //Act
+        
+        //Assert
+    }
+
+
+}
