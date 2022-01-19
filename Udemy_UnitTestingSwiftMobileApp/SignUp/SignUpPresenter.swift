b50116168fd4e90823641eec7f58c57f5f91cf35
JunYoungJee <rainmaker8492@gmail.com>
2022-01-20 01:41:57 +0900

    61

---
+++ Udemy_UnitTestingSwiftMobileApp/SignUp/SignUpPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpPresenter.swift
+//  Udemy_UnitTestingSwiftMobileApp
+//
+//  Created by pablo.jee on 2022/01/20.
+//
+
+import Foundation
+
+class SignUpPresenter {
+    
+    func processUserSignUp(formModel: SignupFormModel) {
+        
+    }
+}
