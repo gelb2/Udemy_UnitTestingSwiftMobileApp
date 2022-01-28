22adcba7a461fd3485648660ce8a8c410e189708
JunYoungJee <rainmaker8492@gmail.com>
2022-01-29 00:15:00 +0900

    99

---
+++ Udemy_UnitTestingSwiftMobileApp/SignUp/Controllers/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Udemy_UnitTestingSwiftMobileApp
+//
+//  Created by pablo.jee on 2022/01/29.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
